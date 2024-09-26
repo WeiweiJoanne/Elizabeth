a7d88b67eb8c7c2e7b28e212fb9f5e8c5949e356
Joanne Wei <joannewei0921@gmail.com>
2024-09-26 14:06:46 +0800

    update homepage banner

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,20 @@
 // Put your application javascript here
+const nav = document.querySelector(".nav");
+const navBtn = document.querySelectorAll(".nav-button");
+console.log("🚀 ~ navBtn:", navBtn)
+
+const handleScroll = () => {
+    if (window.scrollY > 0) {
+        nav.classList.remove("my-bg-transparent");
+        navBtn.forEach(el => {
+            el.classList.add("text-slate-950")
+        })
+    }else{
+         nav.classList.add("my-bg-transparent");
+        navBtn.forEach((el) => {
+          el.classList.remove("text-slate-950");
+        });
+    }
+}
+
+window.addEventListener("scroll", handleScroll)
